24cdb7c954b8fc871fe1e462fc1c2bd19810827d
haosdent <haosdent@gmail.com>
2013-04-22 19:11:20 +0800

    增加baidu的api.

--- search.js
+++ search.js
@@ -1,18 +1,18 @@
 var http = require('http');
 var util = require('util');
-var debug = require('debug')('Crawler');
-var debug_etao = require('debug')('360');
+//var debug = require('debug')('Crawler');
+//var debug_etao = require('debug')('360');
 var cheerio = require('cheerio');
 
 var Crawler = function(){};
 Crawler.workers = [];
 
 Crawler.search = function(str, cb){
-  debug('will search: ', str);
+  //debug('will search: ', str);
 
   var content = [];
   Crawler.workers.forEach(function(worker){
-    debug('search ['+ worker.name +'] by {' + str + '}');
+    //debug('search ['+ worker.name +'] by {' + str + '}');
     worker.search(str, cb);
   });
 };
@@ -39,20 +39,70 @@ Crawler_360.prototype.search = function(str, cb){
     var dl = $('dl');
     if(dl.length > 0){
       if(dl.length > 3){
-        debug_etao(str, "too many");
+        //debug_etao(str, "too many");
         cb("none");
       }else{
         var res = $('dl>dt').html() + ", " + $('dl dd').find('span').html() + ". " + $('dl dd').find('p').html();
-        debug_etao(str, "got");
+        //debug_etao(str, "got");
         cb(null, '[360报平安]' + res + '\n');
       }
     }
     else{
-      debug_etao(str, "empty");
+      //debug_etao(str, "empty");
       cb("none");
     }
   });
 
+};
+
+/******/
+var Crawler_baidu = function(){
+  this.name = 'baidu';
+};
+
+Crawler_baidu.prototype.search = function(str, cb){
+    var pattern = 'http://opendata.baidu.com/api.php?resource_id=6109&format=json&ie=utf-8&oe=utf-8&query=' + str + '&from_mid=1';
+    var that = this;
+    // debug_etao('url', pattern);
+
+    require('request')({uri: pattern}, function(err, resp, body){
+        // debug_etao('error', err);
+        // debug_etao('statusCode', resp.statusCode);
+        // console.log(body);
+
+        body = JSON.parse(body);
+        var data = body.data[0];
+        var users = data.disp_data;
+        var userStr = that.format(users);
+        if(userStr.length > 1000){
+            userStr = '与您查询相匹配的结果有' + users.length + '个, 因微信API限制, 无法一一列出, 请直接在电脑中打开"http://www.baidu.com/s?wd=%E9%9B%85%E5%AE%89"查询所有结果';
+        };
+        cb(null, userStr);
+    });
+};
+
+Crawler_baidu.prototype.format = function(users){
+    var userStr = '';
+    for(var i = 0, l = users.length; i < l; i++){
+        var user = users[i];
+        if(user.found === '0'){
+            user.found = '未找到';
+        }else{
+            user.found = '已经找到';
+        };
+        userStr += '[姓名]:' + user['name'] + ', ' + user['found'] + '\n';
+        userStr += '[信息来源]:' + user['url'] + '\n';
+        userStr += '[年龄性别]:' + user['age'] + ', ' + user['sex'] + '\n';
+        userStr += '[描述]:' + user['desc'] + '\n';
+        userStr += '[联系方式]:' + user['remarks'] + ', ' + user['phone'] + '\n';
+        if(i < l - 1){
+            userStr += '------------\n';
+        };
+    };
+    if(users.length === 0){
+        userStr = '暂时未查询到相关信息. 此工具使用方法, 直接输入精确的用户名查询，如: 林君杰';
+    };
+    return userStr;
 };
 
 // var c_360 = new Crawler_360('360_i_am_safe');
@@ -64,3 +114,4 @@ Crawler_360.prototype.search = function(str, cb){
 
 module.exports.Crawler = Crawler;
 module.exports.Crawler_360 = Crawler_360;
+module.exports.Crawler_baidu = Crawler_baidu;
--- yaan.js
+++ yaan.js
@@ -5,22 +5,19 @@ var express = require('express');
 
 var Crawler = require('./search').Crawler;
 var Crawler_360 = require('./search').Crawler_360;
+var Crawler_baidu = require('./search').Crawler_baidu;
 
 var c_360 = new Crawler_360();
-Crawler.register(c_360);
+var c_baidu = new Crawler_baidu();
+//Crawler.register(c_360);
+Crawler.register(c_baidu);
 
 
 
 var app = express();
 app.use(express.query());
 
-log = function(_type, _str) {
-  return fs.appendFile("./" + _type + ".log", "" + (new Date) + " " + _str + " \n", function(_err) {
-    if (_err) {
-      return console.log("fs.appendFile err: " + _err + " " + _type + " " + _str);
-    }
-  });
-};
+
 
 var welcome = function(res){
   res.reply('我们暂时切换到<雅安地震寻人搜索整合>频道，输入姓名，将整合互联网各大报平安服务的数据。目前已接入平台：\n360报平安平台');
